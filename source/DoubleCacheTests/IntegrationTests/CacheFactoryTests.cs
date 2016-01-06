68a7642c3b2181fff4feeb639320b09135763920
Harald Schult Ulriksen <harald@ulriksen.net>
2016-01-06 19:31:09 +0100

    Factory with tests

---
+++ source/DoubleCacheTests/IntegrationTests/CacheFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using DoubleCache;
+using DoubleCache.Serialization;
+using FakeItEasy;
+using Shouldly;
+using StackExchange.Redis;
+using Xunit;
+
+namespace DoubleCacheTests.IntegrationTests
+{
+    public class CacheFactoryTests
+    {
+        [Fact]
+        public void CreatesSyncedCache_ReturnsCacheObject()
+        {
+            var connection = A.Fake<IConnectionMultiplexer>();
+            var serializer = A.Fake<IItemSerializer>();
+
+            var cache1 = CacheFactory.CreatePubSubDoubleCache(connection, serializer);
+
+            cache1.ShouldBeOfType<DoubleCache.DoubleCache>();
+        }
+    }
+}
