d148f2788f16ec99553eef1a1acba1b072d9c809
Harald Schult Ulriksen <harald@ulriksen.net>
2016-10-14 14:48:49 +0200

    Bump version for test fixes

--- build.fsx
+++ build.fsx
@@ -12,7 +12,7 @@ let nugetDir = "./nuget"
 let references  = !! "source/DoubleCache/*.csproj"
 let testReferences = !! "source/DoubleCacheTests/*.csproj"    
 
-let version = "1.5.0"
+let version = "1.5.1"
 let commitHash = Information.getCurrentSHA1(".")
 
 let projectName = "DoubleCache"
